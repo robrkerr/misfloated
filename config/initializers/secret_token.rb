6def92502758b8e8dc3dcc8fe52f471515927b4a
Robert Kerr <robkerr87@gmail.com>
2013-11-23 17:04:25 +1100

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Misquopes::Application.config.secret_token = '1b4294bdaf8c0a44e73911b3cf4c3476efdecaf9a6d0cf135ca6dc3d00a139d03279666acf0a216d6aa0a71cdcdd64ae8f7527f972e3f0cd2d4ac3714ffddcef'
